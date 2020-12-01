c2237b6c5942455421c791ff3a021bf753726ab8
wufujun2000 <wufujun2000@gmail.com>
2020-12-01 15:31:47 +0800

    测试运行抽帧服务

---
+++ run.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+
+"""
+Source: wufujun 2020-11-25
+"""
+import cv2
+
+from service.camera_service import CameraService
+
+
+Camera = CameraService(
+    name = "your user name",
+    pwd = "your user password",
+    ip = "your ip address:554",
+    camera_brand = "HIKVISION"
+)
+
+#Camera = CameraService()
+Camera.start()
+fps = Camera.cap.get(cv2.CAP_PROP_CONVERT_RGB)
+print(fps)
+while True:
+    frame = Camera.get_frame()
+    cv2.imshow('test', frame)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
